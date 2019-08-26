0e80326673fb7461ea507877d3840e875ef425af
daniel.obara <daniel.tsutomu@hotmail.com>
2019-08-26 13:24:12 -0300

    Multer and fileController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,16 +4,27 @@
  * */
 
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
+// Rotas sem autenticação
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+// A partir daqui as rotas abaixo passam a precisar da autenticação.
 routes.use(authMiddleware);
+
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
